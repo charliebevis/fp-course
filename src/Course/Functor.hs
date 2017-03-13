8a80bd401918f5e018fadc21806987105528540b
Charlie Bevis <charliebevis@gmail.com>
2017-03-13 14:33:26 -0600

    complete the functor module

--- src/Course/Functor.hs
+++ src/Course/Functor.hs
@@ -41,8 +41,8 @@ instance Functor Id where
     (a -> b)
     -> Id a
     -> Id b
-  (<$>) =
-    error "todo: Course.Functor (<$>)#instance Id"
+  (<$>) f (Id a) =
+    Id (f a)
 
 -- | Maps a function on the List functor.
 --
@@ -56,8 +56,8 @@ instance Functor List where
     (a -> b)
     -> List a
     -> List b
-  (<$>) =
-    error "todo: Course.Functor (<$>)#instance List"
+  (<$>) _ Nil = Nil
+  (<$>) f (x:.xs) = (f x) :. (f <$> xs)
 
 -- | Maps a function on the Optional functor.
 --
@@ -71,8 +71,9 @@ instance Functor Optional where
     (a -> b)
     -> Optional a
     -> Optional b
-  (<$>) =
-    error "todo: Course.Functor (<$>)#instance Optional"
+  (<$>) _ Empty = Empty
+  (<$>) f (Full a) = Full (f a)
+
 
 -- | Maps a function on the reader ((->) t) functor.
 --
@@ -84,7 +85,7 @@ instance Functor ((->) t) where
     -> ((->) t a)
     -> ((->) t b)
   (<$>) =
-    error "todo: Course.Functor (<$>)#((->) t)"
+        (.)
 
 -- | Anonymous map. Maps a constant value on a functor.
 --
@@ -99,8 +100,8 @@ instance Functor ((->) t) where
   a
   -> f b
   -> f a
-(<$) =
-  error "todo: Course.Functor#(<$)"
+(<$) constant =
+  (<$>) (\_ -> constant)
 
 -- | Anonymous map producing unit value.
 --
@@ -120,7 +121,7 @@ void ::
   f a
   -> f ()
 void =
-  error "todo: Course.Functor#void"
+  (<$>) (\_ -> ())
 
 -----------------------
 -- SUPPORT LIBRARIES --
